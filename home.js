c3b911ad517a3c7e8b591697bdd3b80eccb02d28
PirateX20 <feliperangelm16dsm@gmail.com>
2023-04-27 10:39:25 -0300

    0.2

--- home.js
+++ home.js
@@ -1,6 +1,9 @@
 const menuButton = document.getElementById('menu-button');
 const menu = document.getElementById('menu');
 
+console.log("home.js sucessfully loaded!")
+
 menuButton.addEventListener('click', function() {
+  console.log('menuButton okay!')
   menu.classList.toggle('hidden-menu');
 });
