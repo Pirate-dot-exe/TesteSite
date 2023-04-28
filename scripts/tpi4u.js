4309af70ab0af00ab039d14fa096d6290337e574
PirateX20 <feliperangelm16dsm@gmail.com>
2023-04-28 13:25:47 -0300

    v0.4.1 carrossel 50%

--- scripts/tpi4u.js
+++ scripts/tpi4u.js
@@ -1,17 +1,16 @@
 const carrossel = document.querySelector('.carrossel');
-const imgs = carrossel.querySelectorAll('img');
-let currentImg = 0;
-
-function nextImage(){
-    if (currentImg === imgs.length - 1) {
-        currentImg = 0;
-      } else {
-        currentImg++;
-      }
-    setTransform();
+const images = carrossel.querySelectorAll('img');
+let currentIndex = 0;
+function nextImage() {
+  images[currentIndex].addEventListener('transitionend', () => {
+    images[currentIndex].classList.remove('active');
+  });
+  currentIndex++;
+  if (currentIndex >= images.length) {
+    currentIndex = 0;
+  }
+  images[currentIndex].classList.add('active');
+  carrossel.style.transform = `translateX(-${currentIndex * 100}%)`;
 }
 
-function setTransform() {
-    carrossel.style.transform = `translateX(-${currentImg * 100}%)`;
-}
 setInterval(nextImage, 2500);
